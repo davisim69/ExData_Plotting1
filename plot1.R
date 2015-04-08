ee5f2456522f73b52687b38d88137563cae3ea9c
davisim69 <davisim69@gmail.com>
2015-04-08 19:55:46 +0100

    Small modifications to first plot code

--- plot1.R
+++ plot1.R
@@ -1,13 +1,11 @@
 
 library(dplyr)
 setwd("C:/Users/Ian/Documents/DataScience/ExploratoryAnalysis/Data")
-plotData<-read.table(paste(getwd(),"/","household_power_consumption.txt",sep = ""), header=TRUE, sep=";", stringsAsFactors = FALSE)
-
+plotData<-read.table(paste(getwd(),"/","household_power_consumption.txt",sep = ""), 
+                     header=TRUE, sep=";", stringsAsFactors = FALSE, na.strings="?"
+                     ,colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 #Tidy up the variable names
 names(plotData)<-c("Date","Time","GlobalActivePower","GlobalReactivePower","Voltage","GlobalIntensity","SubMetering1","SubMetering2","SubMetering3")
-
-#Treat missing values  "?"
-
 
 
 #Convert the variables into appropriate classes
