d8f71185b83c843f2ce9197702ddb5b15afc2285
davisim69 <davisim69@gmail.com>
2015-04-11 08:20:46 +0100

    Completed plot 1 code and png file

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 
-library(dplyr)
+
 setwd("C:/Users/Ian/Documents/DataScience/ExploratoryAnalysis/Data")
 plotData<-read.table(paste(getwd(),"/","household_power_consumption.txt",sep = ""), 
                      header=TRUE, sep=";", stringsAsFactors = FALSE, na.strings="?"
@@ -8,9 +8,18 @@ plotData<-read.table(paste(getwd(),"/","household_power_consumption.txt",sep = "
 names(plotData)<-c("Date","Time","GlobalActivePower","GlobalReactivePower","Voltage","GlobalIntensity","SubMetering1","SubMetering2","SubMetering3")
 
 
-#Convert the variables into appropriate classes
+#Convert the date and time variables
+plotData$DateTime<-paste(plotData$Date,plotData$Time)
 
+plotData$DateTime<-strptime(plotData$DateTime, format="%d/%m/%Y %H:%M:%S")
 
 
 #Subset the data
-plotData1<-
+plotData1<-subset(plotData,as.Date(plotData$DateTime) >= "2007-02-01" & as.Date(plotData$DateTime) <= "2007-02-02")
+
+#Plot histogram of Global Active Power
+with(plotData1,hist(GlobalActivePower, main="Global Active Power", xlab="Global Active Power (kilowats)", col="red"))
+
+#Copy graph to .png file
+dev.copy(png,file="plot1.png")
+dev.off()
